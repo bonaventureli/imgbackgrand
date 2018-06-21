849086893fa631a94533a3473400d791969cbcd0
bonaventureli <lfwendula@msn.cn>
2018-06-20 19:00:45 -0700

    app

---
+++ mainwindow.cpp
@@ -0,0 +1,22 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDir>
+#include <QWSServer>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+setWindowFlags(Qt::FramelessWindowHint);//set no top widge
+QPalette pal;
+QString filename = QDir::currentPath()+ "/img/456.png";
+QPixmap pixmap(filename);
+pal.setBrush(QPalette::Window,QBrush(pixmap));
+setPalette(pal);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
